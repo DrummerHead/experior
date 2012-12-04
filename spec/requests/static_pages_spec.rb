b5b4c3ab244b476f4fdfbf9ed851cfa2132f4c35
DrummerHead <drummerhead@gmail.com>
2012-12-04 19:27:50 -0200

    Make tests, create about

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+    it "should have a monkey jumping on your face with a + sign marked in fire on its forehead, signifying the help that the user is about to get" do
+      visit '/static_pages/help'
+      page.should have_content('Nurse Monkey will now jump on your face. Have a nice day')
+    end
+  end
+
+  describe "About page" do
+    it "Should tell you the meaning of life" do
+      visit '/static_pages/about'
+      page.should have_content('The Purpose of Human Life is to Create, both New Knowledge and New Experiences.')
+    end
+  end
+end
