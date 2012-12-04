13467657a4a38fd4d6a7929a5c01b91e7d61e01b
DrummerHead <drummerhead@gmail.com>
2012-12-04 21:51:05 -0200

    Create helper for better page Title handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # returns posta title... it doesn't "return" anything but ruby style lalala
+  def posta_title(page_title)
+    base_title = "Magia interpoladax"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
