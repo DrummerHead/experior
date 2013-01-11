3987017bd610304a9bac28466d2c5705ab08010d
DrummerHead <drummerhead@gmail.com>
2013-01-11 21:36:53 -0200

    Sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase}
+  before_save :create_remember_token
 
   validates :name, presence: true, length: {maximum: 50}
 
@@ -27,4 +28,9 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 6}
 
   validates :password_confirmation, presence: true
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
